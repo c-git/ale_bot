573f023bb1e2ebbeaebfd9b8019755f90bb5bb8d
One <43485962+c-git@users.noreply.github.com>
2025-05-11 17:07:21 -0400

    feat: setup startup configuration

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ Proof of Concept discord bot for [A Life Engineered](https://www.youtube.com/@AL
 - [ ] Randomly pair them up
   - [ ] Avoiding people being re-paired with people they've been paired with before
 - [ ] Post the pairs on the 1st of the month
+
+# Configuration
+
+- `TOKEN` - provides the discord token.
+- `TEST_GUILD_ID` - provides the GUILD ID of the test server and enables test mode.
+  When in test mode slash commands are only registered on the test server not globally because they are available more quickly for testing.
+- `STARTUP_MSG_CHANNEL` - If set bot will send a message in this channel when it starts up.
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 mod commands;
+mod startup;
 
-use commands::commands_list;
+use anyhow::Context as _;
+use commands::{Data, commands_list};
 use poise::serenity_prelude as serenity;
-use std::env::var;
-use tracing::info;
+use startup::StartupConfig;
+use tracing::{info, warn};
 use tracing_subscriber::{
     EnvFilter,
     fmt::{self, format::FmtSpan},
@@ -21,6 +23,9 @@ pub async fn start_bot() {
 
     info!("Bot version is {}", env!("CARGO_PKG_VERSION"));
 
+    // Load startup configuration
+    let startup_config = StartupConfig::new();
+
     // FrameworkOptions contains all of poise's configuration option in one struct
     // Every option can be omitted to use its default value
     let options = poise::FrameworkOptions {
@@ -31,20 +36,44 @@ pub async fn start_bot() {
     let framework = poise::Framework::builder()
         .setup(move |ctx, ready, framework| {
             Box::pin(async move {
-                let connect_msg = format!(
-                    "{} is connected! Version: {}",
-                    ready.user.name,
-                    env!("CARGO_PKG_VERSION")
-                );
+                if startup_config.is_production() {
+                    info!("Production run detected going to register commands globally");
+                    poise::builtins::register_globally(ctx, &framework.options().commands)
+                        .await
+                        .context("failed to register the bot globally")?;
+                } else if let Some(guild_id) = startup_config.test_guild_id {
+                    info!("Development run detected going to register commands for guild: {guild_id}");
+                    poise::builtins::register_in_guild(
+                        ctx,
+                        &framework.options().commands,
+                        guild_id,
+                    )
+                    .await
+                    .with_context(|| {
+                        format!(
+                            "failed to register slash commands for {:?} in guild: {guild_id}",
+                            ready.user.name
+                        )
+                    })?;
+                } else {
+                    unreachable!("Assumption that providing a test guild id determines if it is a test run violated")
+                }
+                let connect_msg = format!("{} is connected! Version: {}", ready.user.name, env!("CARGO_PKG_VERSION"));
                 info!("{connect_msg}");
-                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
-                Ok(commands::Data::default())
+                if let Some(channel) = startup_config.bot_startup_channel{
+                    channel.say(ctx, connect_msg).await?;
+                } else{
+                    warn!("Not sending connection notification because `bot_startup_channel` not set");
+                }
+                info!("END OF SETUP CLOSURE");
+                Ok(Data::default())
             })
         })
         .options(options)
         .build();
 
-    let token = var("TOKEN").expect("Missing `TOKEN` env var, see README for more information.");
+    let token =
+        std::env::var("TOKEN").expect("Missing `TOKEN` env var, see README for more information.");
     let intents = serenity::GatewayIntents::non_privileged();
 
     let client = serenity::ClientBuilder::new(token, intents)
