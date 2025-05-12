9c2e6703a0091ea6f3cdcae7df391eec4cfee70f
One <43485962+c-git@users.noreply.github.com>
2025-05-11 23:55:13 -0400

    feat: remove ideas functionality

--- src/commands/unranked_cmd.rs
+++ src/commands/unranked_cmd.rs
@@ -1,21 +1,14 @@
 //! Groups the commands related to the unranked challenge
 
 use poise::serenity_prelude::{CacheHttp, ChannelId};
-use tracing::{info, instrument};
+use tracing::instrument;
 
-use self::{idea::idea, score::score};
+use self::score::score;
 use crate::{
     Context, Data,
-    commands::{
-        call_to_parent_command, is_auth, tracing_handler_end, tracing_handler_start,
-        unranked_cmd::{
-            idea::{display_ideas_channel, do_ideas_reset},
-            score::{display_scores_channel, do_scores_reset},
-        },
-    },
+    commands::{call_to_parent_command, is_auth, tracing_handler_end, tracing_handler_start},
 };
 
-mod idea;
 mod score;
 
 #[poise::command(
@@ -24,7 +17,7 @@ mod score;
     track_edits,
     aliases("ur"),
     subcommand_required,
-    subcommands("idea", "score", "start_event")
+    subcommands("score", "start_event")
 )]
 #[instrument(name = "unranked", skip(ctx))]
 /// Commands related to the Unranked Challenge [aliases("ur")]
@@ -42,56 +35,11 @@ pub async fn start_event(ctx: Context<'_>) -> anyhow::Result<()> {
     tracing_handler_end()
 }
 
-#[instrument(skip(cache_http, data))]
+#[instrument(skip(_cache_http, _data))]
 pub async fn do_start_event(
-    cache_http: impl CacheHttp,
+    _cache_http: impl CacheHttp,
     channel_id: ChannelId,
-    data: &Data,
+    _data: &Data,
 ) -> anyhow::Result<()> {
-    info!("START");
-    channel_id
-        .say(
-            &cache_http,
-            "Setting up for the start of a new unranked event",
-        )
-        .await?;
-
-    display_ideas_channel(&cache_http, channel_id, data, true).await?;
-
-    // Get the leading idea (winning at this point as it's the end) and the ones above the threshold
-    let leading = data.inner.unranked.ideas_pop_leading()?;
-    channel_id
-        .say(&cache_http, "Extracting leading idea")
-        .await?;
-
-    // Do resets
-    do_ideas_reset(&cache_http, channel_id, data).await?;
-    do_scores_reset(&cache_http, channel_id, data).await?;
-
-    // Get message for new scores
-    let msg = if let Some(idea) = leading {
-        idea.description
-    } else {
-        "Seems there were no ideas".to_string()
-    };
-
-    // Set message for new scores
-    data.inner
-        .unranked
-        .scores_message(Default::default(), msg.clone())?;
-
-    display_scores_channel(&cache_http, channel_id, data).await?;
-
-    channel_id
-        .say(
-            &cache_http,
-            format!("@here This season we will be doing:\n---\n> {msg}\n---"),
-        )
-        .await?;
-
-    channel_id
-        .say(&cache_http, "@here Setup successfully completed GLHF")
-        .await?;
-
-    tracing_handler_end()
+    todo!()
 }
--- src/model/unranked.rs
+++ src/model/unranked.rs
@@ -1,25 +1,18 @@
 //! Groups the functionality related to unranked business logic
 
-use crate::{
-    config::SharedConfig,
-    model::unranked::{ideas::Ideas, scores::Scores},
-};
+use crate::{config::SharedConfig, model::unranked::scores::Scores};
 use std::sync::{Arc, Mutex};
 
-pub mod ideas;
 pub mod scores;
 
 pub struct Unranked {
-    ideas: Arc<Mutex<Ideas>>,
     scores: Arc<Mutex<Scores>>,
     shared_config: &'static SharedConfig,
 }
 impl Unranked {
     pub async fn new(shared_config: &'static SharedConfig) -> Self {
-        let ideas = Arc::new(Mutex::new(Ideas::new(shared_config).await));
         let scores = Arc::new(Mutex::new(Scores::new(shared_config).await));
         Self {
-            ideas,
             scores,
             shared_config,
         }
